c45509e45b97b3a3ad41a7732817eaa2a28c06f4
Huy Duong <duonghuy1401@gmail.com>
2020-09-08 22:11:27 +0700

    Finish blog v1

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,9 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
+    title: `Viết truyện nhảm, tản mạn...`,
     author: {
-      name: `Kyle Mathews`,
-      summary: `who lives and works in San Francisco building useful things.`,
+      name: `Huy Dương`,
+      summary: `Học IT nhưng thích viết nhảm viết xàm về đủ mọi thứ trên đời.`,
     },
     description: `A starter blog demonstrating what Gatsby can do.`,
     siteUrl: `https://gatsby-starter-blog-demo.netlify.app/`,
