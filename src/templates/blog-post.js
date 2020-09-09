70e09f909d9635332b810217f953d1a746772141
Huy Duong <duonghuy1401@gmail.com>
2020-09-09 12:01:27 +0700

    Small change

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -12,7 +12,7 @@ const BlogPostTemplate = ({ data, pageContext, location }) => {
   const siteTitle = data.site.siteMetadata.title
   const { previous, next, slug } = pageContext
   const disqusConfig = {
-    shortname: "hduongsblog",
+    shortname: process.env.GATSBY_DISQUS_NAME,
     config: { identifier: slug, title: siteTitle },
   }
 
